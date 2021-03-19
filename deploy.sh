541489dca28cfc08970c669fe940a45fdfff6d19
John <johnl@delta-n.nl>
2021-03-19 16:23:25 +0100

    Password generator

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ az deployment group create --resource-group $rgName --template-uri $templateLoca
 
 # Get ObjectId for user
  az ad user show --id "johnl@delta-n.nl" --query "objectId"
+
+# Generate password
+password=$(openssl rand -base64 10)
