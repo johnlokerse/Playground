bda57dfd39070ce22b84c2a165e44932a792f8e9
John <J.G.Lokerse@gmail.com>
2021-03-24 19:34:30 +0000

    Added objectId param

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ source ./utils.sh
 az group create --name $rgName --location $location
 
 # Get ObjectId for user
-object_id=$(get_objectid "johnl@delta-n.nl")
+object_id=$(get_objectid "johnl@delt`a-n.nl")
 
 # Generate password
 password=$(generate_password 10)
 
-az deployment group create --resource-group $rgName --template-uri $templateLocation --parameters adminPassword=$password
+az deployment group create --resource-group $rgName --template-uri $templateLocation --parameters adminPassword=$password userObjectId=$object_id
