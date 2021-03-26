7a6c3583b59da46bde3cf74880f8c9871e689f14
John <johnl@delta-n.nl>
2021-03-26 16:35:10 +0100

    Added parameters files

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 object_id=$1
-vmName=$2
-addressPrefix=$3
 
 # Init script variables
 source ./variables.sh
@@ -13,12 +11,8 @@ az group create --name $rgName --location $location
 # Generate password
 password=$(generate_password 10)
 
-echo $1
-echo $2
-echo $3
-
 header "Validating ARM template"
-validate_arm=$(az deployment group validate --resource-group $rgName --template-file $templateLocation --parameters prefix=$vmName adminPassword=$password userObjectId=$object_id vnetAddressPrefix=$addressPrefix)
+validate_arm=$(az deployment group validate --resource-group $rgName --template-file $templateLocation --parameters $parameterLocation --parameters adminPassword=$password userObjectId=$object_id)
 if $? -gt 0 &> /dev/null; then
     header "Exiting... validation failed."
     exit 1;
@@ -26,4 +20,4 @@ fi
 echo "validation done - OK"
 
 header "Deploying ARM template"
-az deployment group create --resource-group $rgName --template-file $templateLocation --parameters prefix=$vmName adminPassword=$password userObjectId=$object_id vnetAddressPrefix=$addressPrefix
+az deployment group create --resource-group $rgName --template-file $templateLocation --parameters $parameterLocation --parameters adminPassword=$password userObjectId=$object_id
