f25d70401c60015defe9028d9354aadaf09bf4c9
John <johnl@delta-n.nl>
2021-03-26 10:51:13 +0100

    Added validations for ARM template

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,12 @@ object_id=$(get_objectid "johnl@delta-n.nl")
 # Generate password
 password=$(generate_password 10)
 
+header "Validating ARM template.."
+validate_arm=$(az deployment group validate --resource-group $rgName --template-file $templateLocation --parameters adminPassword=$password userObjectId=$object_id)
+if $? -gt 0 &> /dev/null; then
+    header "Exiting... validation failed."
+    exit 1;
+fi
+
+header "Deploying arm template"
 az deployment group create --resource-group $rgName --template-uri $templateLocation --parameters adminPassword=$password userObjectId=$object_id
